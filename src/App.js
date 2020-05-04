0b1805aa99b6dedb7d572533313caccbe2e8999a
mrChernicharo <felipe.chernicharo@gmail.com>
2020-05-04 17:21:54 -0300

    window-size

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Playground from './Playground';
 import Paint from './components/Paint';
+import WindowSize from './components/WindowSize';
 
 function App() {
   return (
     <div className="App">
         <Paint />
         <Playground />
+        <WindowSize />
     </div>
 
   );
--- src/components/WindowSize.js
+++ src/components/WindowSize.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
 export default function WindowSize() {
-  const [[windowX, windowY], setWindowSize] = useState([window.innerWidth, window.innerHeight])
+  const [[windowWidth, windowHeight], setWindowSize] = useState([window.innerWidth, window.innerHeight])
   const [visible, setVisible] = useState(false)
 
   useEffect(() => {
     let timeoutId
     const handleResize = () => {
-      setWindowSize([windowX, windowY]);
+      setWindowSize([window.innerWidth, window.innerHeight]);
       setVisible(true);
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => setVisible(false), 1000);
     }
-    window.addEventListener('resize',handleResize)
+    window.addEventListener('resize',handleResize);
+    return () => window.removeEventListener('resize', handleResize)
   }, []);
 
   return (
